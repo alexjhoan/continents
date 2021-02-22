fec2d75592b2a4eb09b06193121ee86f46a626a9
alexjhoan <alexjhoan.25@gmail.com>
2021-02-22 18:10:43 -0400

    done banner, navbar and whatsapp icon 1815 22022021

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,20 @@
-console.log("conected")
-
 new WOW().init();
+
+//---------------------------------bgNavBar----------------------------------
+
+function bgNavBar() {
+  let container = $("#banner").height();
+  let scroll = $(window).scrollTop();
+  if (scroll > container) {
+    $(".bgheader").addClass("bgShow")
+  } else {
+    $(".bgheader").removeClass("bgShow")
+  }
+}
+
+// ------------------------------controls-scroll----------------------------
+
+
+$(window).on("scroll", function () {
+  bgNavBar()
+});
