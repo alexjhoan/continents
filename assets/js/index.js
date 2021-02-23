b09689d909495ebd6891295b3d8fea17fd87b1d6
alexjhoan <alexjhoan.25@gmail.com>
2021-02-23 17:56:24 -0400

    done us section 1800 23022021

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ new WOW().init();
 function bgNavBar() {
   let container = $("#banner").height();
   let scroll = $(window).scrollTop();
-  if (scroll > container) {
+  if (scroll > (container - 66)) {
     $(".bgheader").addClass("bgShow")
   } else {
     $(".bgheader").removeClass("bgShow")
@@ -14,7 +14,29 @@ function bgNavBar() {
 
 // ------------------------------controls-scroll----------------------------
 
-
 $(window).on("scroll", function () {
   bgNavBar()
 });
+
+// ------------------------------controls-scroll----------------------------
+
+if (screen.width < 992) {
+  $("#us5 .containerGrid").addClass("owl-carousel owl-theme")
+} else {
+  $("#us5 .containerGrid").removeClass("owl-carousel owl-theme")
+}
+
+$('.owl-carousel').owlCarousel({
+  loop:false,
+  margin:0,
+  nav:false,
+  dots: true,
+  responsive:{
+    0:{
+        items:1
+    },
+    600:{
+        items:3
+    },
+  }
+})
