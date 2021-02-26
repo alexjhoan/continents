6add2fd0f48e448a20ce44b3b70cd4426c3685b9
anaismiraira <mirairaanais@gmail.com>
2021-02-25 22:21:31 -0430

    json

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,3 +40,14 @@ $('.owl-carousel').owlCarousel({
     },
   }
 })
+
+// ------------------------------aniamtions graphic----------------------------
+
+  var animation = bodymovin.loadAnimation({
+  container: document.getElementById('graphic'), // Required
+  path: 'data.json', // Required
+  renderer: 'svg', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "Hello World", // Name for future reference. Optional.
+})
